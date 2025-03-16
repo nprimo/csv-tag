86440f1ee460d5b25dda3e3f33686a14b7fbf8ee
nprimo <primo.niccolo@gmail.com>
2025-03-16 16:54:44 +0000

    docs: update example

--- example/main.go
+++ example/main.go
@@ -13,6 +13,7 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
+	defer f.Close()
 	rows, err := csv.NewReader(f).ReadAll()
 	if err != nil {
 		panic(err)
@@ -28,5 +29,29 @@ func main() {
 		panic(err)
 	}
 
-	fmt.Printf("%+v\n", data)
+	fmt.Println("-- Original")
+	for i, el := range data {
+		fmt.Printf("row %d: %+v\n", i, el)
+	}
+
+	data[0].Word = "guarda"
+	data[1].Word = "come"
+	fmt.Println("-- Modified")
+	for i, el := range data {
+		fmt.Printf("row %d: %+v\n", i, el)
+	}
+
+	rows, err = csvtag.Marshall(data)
+	if err != nil {
+		panic(err)
+	}
+
+	f1, err := os.Create("example_modified.csv")
+	if err != nil {
+		panic(err)
+	}
+	defer f1.Close()
+	if err := csv.NewWriter(f1).WriteAll(rows); err != nil {
+		panic(err)
+	}
 }
